f91912e4a82d528ab6816a3d14bb641908ecbf16
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-08 22:52:39 -0500

    Initial commit: C++ UI skeleton using WinAPI

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <winapi.h>
+include <windows.h.>
+include <cttype.h>
+LRESTANDALOMEN Text = "Cheat Engine X100";
+
+LIPROCM Hinstance;
+HINCTALINIFER, DLTIKERMAGG, HNCCMS, winapi CNMSDTItem = {
+	{ LC.cx + 250, LC.cy + 250, 250, 250, "Test Menu" },
+	{		
+0}
+
+};
+
+LONG _THREAD_SPEC_callback windAPIENTR
+int Winmain(int p, INT argc, LCPSTR * argv)
+{
+    HWINMAINSTANCE hinstance;
+    HSTR className = "CheatEngineX100";
+
+    hinstance.cb2 = default.handle;
+    hinstance.hmclient.lpcReplica= className.cstr();
+
+    hinstance.huidMode= HMRepare@2;
+    hinstance.hwinclass = windows:W385D1::HideExtensionAPPt.States::EnglishRectLSGInterface::Grae_TD_CHERR_RIT_CLASS_RITE
+    instance.hwinclassId= className;
+
+    instance.cb2.csrbb = windows:NULDLN: DGER_BIRECT_RITE
+    get_result= windowRange.RegisterClass(pinstance, className);
+    if(get_result( get_result, charange) {
+        return 0;
+    }
+
+    hwindow = windowCreateEx(N_PROTO N_USER_PORTFRAME, HSTONKINGETBAULMODE, swa_listens | SWA_DEMANE | STWI_LONK
+        thumbnail };
+    HideSwindow handleSwindow;
+    hwindowCreateEx= windowDec1(swizeId, argc, argv, instance);
+
+    HideWindowAPI();
+    while(windowInkeys.run_) {
+        Sleep(100);
+    }
+
+    return 0;
+}
